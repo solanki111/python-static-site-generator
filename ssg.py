23572066a6e63b691b7fe6e9811f2f698bc4bf43
Abhishek <abhishek.singh2@cerner.com>
2020-12-31 09:36:38 +0000

    corrected module1

--- ssg.py
+++ ssg.py
@@ -9,4 +9,4 @@ def main(source="content", dest="dist"):
     Site(**config).build()
 
 
-typer.run(main())
+typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -3,25 +3,16 @@ from pathlib import Path
 class Site:
 
     def __init__(self, source, dest):
-        self._source = source
-        self._dest = dest
-
-    def source(self):
-        return Path(self._source)
-
-    def dest(self):
-        return Path(self._dest)
-
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
-        part1 = path.relative_to(self.source())
-        part2 = self.dest()
-        directory = Path(part1.joinpath(part2))
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
 
     def build(self):
-        self.dest().mkdir(parents=True, exist_ok=True)
-        for path in self.source().rglob("*"):
-            if Path(path).is_dir:
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
                 self.create_dir(path)
