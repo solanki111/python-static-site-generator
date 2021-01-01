1bcb7bb5bd670bb437b42c6eb9763220bcd63748
Abhishek <abhishek.singh2@cerner.com>
2021-01-01 21:20:39 +0000

    corrected module3

--- ssg/content.py
+++ ssg/content.py
@@ -4,33 +4,47 @@ import re
 
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
 
-    def load(self, cls, string):
-        _, fm, content = self.__regex.split(string[2])
-
-        load(fm, )
-
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
 
     def __init__(self, metadata, content):
         self.data = metadata
-        self.data = {'content': content}
-
+        self.data['content'] = content
 
     @property
+    def body(self):
 
+        return self.data['content']
+
+    @property
+    def type(self):
+        return self.data['type'] if 'type' in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data['type'] =  type
+
+    def __getitem__(self, key):
+        return self.data[key]
 
     def __iter__(self):
-        return self.data
+        return self.data.__iter__()
 
     def __len__(self):
         return len(self.data)
 
     def __repr__(self):
         data = {}
-        str(data)
+        for key, value in self.data.items():
+            if key != 'content':
+                data[key] = value
+        return str(data)
 
